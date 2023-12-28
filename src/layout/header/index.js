25a48cfc1dbe89ef4273cb9ed4a31a29dc639e52
apurv-zappian <apurv.roy@zappian.com>
2023-12-28 17:52:28 +0530

    update header

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   Box,
   CardMedia,
   Container,
+  Grid,
   Link,
   Stack,
   Toolbar,
+  Typography,
 } from "@mui/material";
 import CredmudraLogo from "../../assets/images/credmudra_logo_new.png";
 import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
 import MenuIcon from "@mui/icons-material/Menu";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const [showBox, setShowBox] = useState(false);
+
+  const handleLogoClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+    navigate("/");
+  };
+
+  const handleBusinessLoanClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+    navigate("/business-loan");
+  };
+
+  const handlePersonalLoanHover = () => {
+    setShowBox(true);
+  };
+
+  const handlePersonalLoanMouseLeave = () => {
+    setShowBox(false);
+  };
+
+  const handleLinkClick = (to) => {
+    // setShowBox(false);
+    navigate(to);
+  };
+
   return (
     <>
       <Box>
@@ -33,13 +68,20 @@ const Header = () => {
               >
                 <CardMedia
                   component="img"
-                  sx={{ width: { xs: "150px", sm: "170px", md: "200px" } }}
+                  sx={{
+                    width: { xs: "150px", sm: "170px", md: "200px" },
+                    cursor: "pointer",
+                  }}
                   image={CredmudraLogo}
+                  onClick={handleLogoClick}
                 />
                 <Stack
                   direction="row"
                   spacing={6}
-                  sx={{ display: { xs: "none", sm: "none", md: "flex" } }}
+                  sx={{
+                    display: { xs: "none", sm: "none", md: "flex" },
+                    cursor: "pointer",
+                  }}
                 >
                   <Link
                     underline="none"
@@ -47,6 +89,7 @@ const Header = () => {
                     fontWeight="500"
                     fontSize="16"
                     fontFamily="Inter, sans-serif"
+                    onClick={handleBusinessLoanClick}
                   >
                     Business Loan
                   </Link>
@@ -57,10 +100,12 @@ const Header = () => {
                     fontSize="16"
                     fontFamily="Inter, sans-serif"
                     display="flex"
+                    onMouseEnter={handlePersonalLoanHover}
+                    // onMouseLeave={handlePersonalLoanMouseLeave}
                   >
                     Personal Loan
                     <KeyboardArrowDownOutlinedIcon
-                      sx={{ height: "20px", width: "20px", marginTop: "-2px" }}
+                      sx={{ height: "20px", width: "20px" }}
                     />
                   </Link>
                   <Link
@@ -91,6 +136,181 @@ const Header = () => {
           </Container>
         </AppBar>
       </Box>
+
+      {showBox && (
+        <Box
+          sx={{
+            height: "230px",
+            width: "1345px",
+            backgroundColor: "#000",
+            position: "fixed",
+            top: "64px",
+            left: "50%",
+            transform: "translateX(-50%)",
+            zIndex: 9999,
+          }}
+          onMouseEnter={handlePersonalLoanHover}
+          onMouseLeave={handlePersonalLoanMouseLeave}
+        >
+          <Grid Container sx={{ display: "flex", width: "100%" }}>
+            <Grid
+              item={4}
+              sx={{
+                bgcolor: "#000",
+                width: "33%",
+                padding: "20px 100px 0 30px",
+              }}
+            >
+              <Typography
+                sx={{
+                  color: "#F7D64A",
+                  fontWeight: 900,
+                  fontFamily: "Inter",
+                  paddingBottom: "10px",
+                }}
+                variant="h5"
+              >
+                Personal Loan
+              </Typography>
+              <Typography sx={{ color: "white" }}>
+                Experience The Convenience Of Getting An Instant Personal Loan
+                Through Our Easy Application Process.
+              </Typography>
+            </Grid>
+            <Grid
+              item={4}
+              sx={{
+                bgcolor: "#000",
+                width: "33%",
+                display: "flex",
+                flexDirection: "column",
+                paddingTop: "30px",
+              }}
+            >
+              <Link
+                onClick={() => handleLinkClick("/personal-loan")}
+                style={{
+                  color: "#fff",
+                  fontFamily: "Inter",
+                  textDecoration: "none",
+                  cursor: "pointer",
+                }}
+              >
+                Personal Loan
+              </Link>
+              <Link
+                onClick={() => handleLinkClick("/personalloanfordebt")}
+                style={{
+                  color: "#fff",
+                  fontFamily: "Inter",
+                  paddingTop: "10px",
+                  textDecoration: "none",
+                  cursor: "pointer",
+                }}
+              >
+                Personal Loan For Debt Consolidation
+              </Link>
+              <Link
+                onClick={() => handleLinkClick("/personalloanformedical")}
+                style={{
+                  color: "#fff",
+                  fontFamily: "Inter",
+                  paddingTop: "10px",
+                  textDecoration: "none",
+                  cursor: "pointer",
+                }}
+              >
+                Personal Loan For Medical Emergencies
+              </Link>
+              <Link
+                to={"/personalloanfortravel"}
+                onClick={() => handleLinkClick("/personalloanfortravel")}
+                style={{
+                  color: "#fff",
+                  fontFamily: "Inter",
+                  paddingTop: "10px",
+                  textDecoration: "none",
+                  cursor: "pointer",
+                }}
+              >
+                Personal Loan For Travel
+              </Link>
+              <Link
+                onClick={() => handleLinkClick("/personalloanfortwowheeler")}
+                style={{
+                  color: "#fff",
+                  fontFamily: "Inter",
+                  paddingTop: "10px",
+                  textDecoration: "none",
+                  cursor: "pointer",
+                }}
+              >
+                Personal Loan For Two Wheeler
+              </Link>
+            </Grid>
+            <Grid
+              item={4}
+              sx={{
+                bgcolor: "#000",
+                width: "33%",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <Link
+                onClick={() =>
+                  handleLinkClick("/personalloanforhomerenovation")
+                }
+                style={{
+                  color: "#fff",
+                  fontFamily: "Inter",
+                  paddingTop: "60px",
+                  textDecoration: "none",
+                  cursor: "pointer",
+                }}
+              >
+                Personal Loan For Home Renovation
+              </Link>
+              <Link
+                onClick={() => handleLinkClick("/personalloaneligibility")}
+                style={{
+                  color: "#fff",
+                  fontFamily: "Inter",
+                  paddingTop: "10px",
+                  textDecoration: "none",
+                  cursor: "pointer",
+                }}
+              >
+                Personal Loan Eligibility
+              </Link>
+              <Link
+                onClick={() => handleLinkClick("/personalloanemicalculator")}
+                style={{
+                  color: "#fff",
+                  fontFamily: "Inter",
+                  paddingTop: "10px",
+                  textDecoration: "none",
+                  cursor: "pointer",
+                }}
+              >
+                Personal Loan Interest Rate
+              </Link>
+              <Link
+                onClick={() => handleLinkClick("/personalloanfortwowheeler")}
+                style={{
+                  color: "#fff",
+                  fontFamily: "Inter",
+                  paddingTop: "10px",
+                  textDecoration: "none",
+                  cursor: "pointer",
+                }}
+              >
+                Personal Loan Interest Rate
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      )}
     </>
   );
 };
